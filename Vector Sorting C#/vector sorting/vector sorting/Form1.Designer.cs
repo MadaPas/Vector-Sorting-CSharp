430ac01295abe5c01b8a92b3bb21339c87605833
MadaPas <44397138+MadaPas@users.noreply.github.com>
2019-03-06 19:37:48 +0100

    Add files via upload

---
+++ Vector Sorting C#/vector sorting/vector sorting/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace sortare_vectori_animat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "n=";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 6);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(308, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Generate the vector";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 116);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Sort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(631, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 36);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 96);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Buble Sort";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(631, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 36);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(528, 46);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(159, 33);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Delete all";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(16, 311);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(109, 36);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Sort";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 292);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Insertion Sort";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(528, 8);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(159, 32);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Sort all";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(774, 394);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Vector Sorting";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Vector Sorting C#/vector sorting/vector sorting/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sortare_vectori_animat
+{
+    public partial class Form1 : Form
+    {
+        TextBox[] t1 = new TextBox[200];
+        TextBox[] t2 = new TextBox[200];
+         int[] v = new int[200];
+         int[] v1 = new int[200];
+        int n,i,j,ii,jj;
+        Random r = new Random(); int ww,nr,alina,nrr,aux,nrmm,wew;
+        Font fontnou = new Font("Microsoft Sans Serif", 20, FontStyle.Bold);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+      
+
+        private void button1_Click(object sender, EventArgs e)// genereaza si afiseaza vector
+        {
+            n = Convert.ToInt32(textBox1.Text);
+            int i;
+            for (i = 1; i <= n; i++)// bule[1]
+            {
+                t1[i] = new TextBox();
+                t1[i].Width = t1[i].Height = 55;
+                t1[i].Multiline = true;
+                t1[i].Top = 100;
+                t1[i].Left = 160 + (i - 1) * 57;
+                v[i] = r.Next(1, 10);
+                v1[i] = v[i];
+                t1[i].Text = Convert.ToString(v[i]);
+                t1[i].Font = fontnou;
+                t1[i].TextAlign = HorizontalAlignment.Center;
+                this.Controls.Add(t1[i]);
+            }
+
+
+            for ( i=1; i<= n; i++)// inserare[2]
+            {
+                t2[i] = new TextBox();
+                t2[i].Width = t2[i].Height = 55;
+                t2[i].Multiline = true;
+                t2[i].Top = 300;
+                t2[i].Left = 160 + (i - 1) * 57;
+                t2[i].Text = Convert.ToString(v[i]);
+                t2[i].Font = fontnou;
+                t2[i].TextAlign = HorizontalAlignment.Center;
+                this.Controls.Add(t2[i]);
+            }
+
+
+        }
+                 
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+           nr = 0; i = 1; j = 2;
+           timer1.Start(); 
+
+
+        }
+   
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (v[i] > v[j])
+            {
+                if (nr == 0)
+                { t1[i].Top--; t1[j].Top++; if (t1[i].Top == 42) nr = 1; }
+                if (nr == 1)
+                { t1[i].Left++; t1[j].Left--; if (t1[i].Left == 160 + (j - 1) * 57) nr = 2; }
+                if (nr == 2)
+                {
+                    t1[i].Top++; t1[j].Top--;
+                    if (t1[i].Top == 100)
+                    {
+                        int aux = v[i];
+                        v[i] = v[j];
+                        v[j] = aux;
+                        nr = 3;
+                        TextBox aux1 = t1[i];
+                        t1[i] = t1[j]; t1[j] = aux1;
+                    }
+                }
+            }
+            if (v[i]<=v[j] || nr == 3)
+                {
+                    nr = 0;
+                    if (j < n) { j++; }
+                    else
+                        if (i < n - 1) { i++; j = i + 1; }
+                        else
+                        {
+                            for (int fin = 1; fin <= n; fin++)
+                                t1[fin].BackColor = Color.LightGreen;
+                            timer1.Stop();
+                        }
+                }   
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for(int ii=1;ii<=n;ii++)
+               this.Controls.Remove(t1[ii]);
+            timer1.Stop();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int ii = 1; ii <= n; ii++)
+                this.Controls.Remove(t2[ii]);
+            timer2.Stop();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            for (int ii = 1; ii <= n; ii++)
+            {
+                this.Controls.Remove(t2[ii]);
+                this.Controls.Remove(t1[ii]);  
+            }
+            timer1.Stop();
+            timer2.Stop();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //v1
+            nrr = 0;
+            ii = 2;
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+            if (nrr == 0)//pas 1
+            {
+                nrmm = 0;//numarul de numere mai mari din v1 fata de v[ii] de pe pozitii mai mici(in stanga)
+                for (int robert = 1; robert < ii; robert++)// se gaseste valoare variabilei nrmm
+                {
+                    if (v1[robert] > v1[ii])
+                        nrmm++;
+                }
+                if (nrmm > 0)//daca sunt numere mai mari decat v1[ii] in stanga lui se insereaza dupa nrmm numere valoare din v1[ii] (in stanga)
+                {
+                    alina = v1[ii];
+                    for (int h = ii; h >= ii - nrmm + 1; h--)
+                    {
+                        v1[h] = v1[h - 1];
+                    }
+                    v1[ii - nrmm] = alina;
+                    nrr = 1;
+                }
+                else//daca nrmm==0 atunci mergem la urmatoare valoade din vector
+                {
+                    if(ii<n)
+                    ii++;
+                    else
+                    {
+
+                        for (int fin = 1; fin <= n; fin++)
+                            t2[fin].BackColor = Color.LightGreen;
+                        timer2.Stop();
+                    }
+                }
+            }
+            
+                if (nrr == 1)//pas 2
+            {
+                if (t2[ii].Top > 243)
+                {
+                    t2[ii].Top--;//v1[ii]urca
+                    for(int mama=ii-nrmm;mama<ii;mama++)
+                        t2[mama].Left++;// cele nrmm numere mai mari decat v1[ii] din stanga acestuia se deplaseaza cu o patratica catre dreapta
+                }
+                else
+                    nrr = 2;//t2[ii] a ajuns la pozitia dorita deci mergem la pasul urmator
+            }
+            if (nrr == 2)//pas 3
+            {
+                if (t2[ii].Left > 160 + (ii - nrmm - 1) * 57)
+                    t2[ii].Left--;// se deplaseaza catre stanga t2[ii] pana la locul corect in vector 
+                else
+                    nrr=3;//pasul urmator
+            }
+            if (nrr == 3)//pas 4
+            {
+                if (t2[ii].Top < 300)
+                    t2[ii].Top++;// coboara t2[ii]
+                else { nrr = 4;  }
+            }
+            if (nrr == 4)//pas 5
+            {
+                // sortarea vectorului de tip textbox, t2 
+                TextBox aalina = t2[ii];
+                for (int h = ii; h >= ii - nrmm + 1; h--)
+                {
+                    t2[h] = t2[h - 1];
+                }
+                t2[ii-nrmm] = aalina;
+
+
+                if (ii == n)
+                {
+
+                    for (int fin = 1; fin <= n; fin++)
+                        t2[fin].BackColor = Color.LightGreen;
+                    timer2.Stop();
+                }
+                if(ii<n)
+                ii++;
+                nrr = 0;
+               
+            }
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            nrr = 0;
+            ii = 2;
+            nr = 0; i = 1; j = 2;
+            timer1.Start();
+            timer2.Start();
+        }
+
+      }
+    }
+
